af3a026e373533fadc6087e03fd0475dfa0f36ac
xiaogeng-dog <359315634@qq.com>
2022-07-15 14:17:01 +0800

    视频mv

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -1,3 +1,5 @@
+import eventStore from '../../store/index'
+
 // pages/home-video/index.js
 Page({
 
@@ -5,62 +7,72 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    topMVList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  onLoad (options) {
+    // 发起数据请求
+    eventStore.dispatch("getTopMVAction", 0)
 
+    // 监听topMV数据
+    eventStore.onState("topMV", this.topMVHandler)
+  },
+
+  topMVHandler: function (res) {
+    this.setData({
+      topMVList: res
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
+  onReady () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {
+  onShow () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
+  onHide () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
+  onUnload () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
+  onPullDownRefresh () {
+    eventStore.dispatch("getTopMVAction", 0)
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
+  onReachBottom () {
+    eventStore.dispatch("getTopMVAction")
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
+  onShareAppMessage () {
 
   }
 })
